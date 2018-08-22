5537d86a9e22072c040ccd52623562fc8e316f1d
Rafael Brigagao Paulino <rafael.paulino@FIESP.COM.BR>
2018-08-22 15:54:19 -0300

    adicionei a biblioteca oficial com o código funcionando

---
+++ medium/examples/credentials.php
@@ -0,0 +1,11 @@
+<?php
+
+$credentials = [
+    'client-id' => '38dec2e44848',
+    'client-secret' => '9b016e885698e4484b5d0b28120b8fc0607f2061',
+    'redirect-url' => 'http://rafathemes.com.br/teste/medium/examples/callback.php',
+    'state' => 'secret',
+    'scopes' => 'basicProfile,publishPost,listPublications',
+];
+
+?>
